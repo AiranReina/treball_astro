0e4be87f1133586436bdf98a6150906cf21e0de8
AiranReina <airanreinadelgado2004@gmail.com>
2025-04-26 16:33:38 +0200

    Analema grafica 2 graficas ahora

--- analema.py
+++ analema.py
@@ -24,24 +24,32 @@ end_date = datetime(2025, 12, 31)
 days = [start_date + timedelta(days=i) for i in range((end_date - start_date).days + 1)]
 
 
-#Registrem els valors de la declinació i l'ascensió recta del Sol per cada día. ...
-#... Realment només ens interessa la declinació, però trobem l'ascensió ...
-#... recta per completitud
+#Registrem els valors de la declinació, l'ascensió recta i l'elevació i azimut del Sol per cada día. ...
+#... Realment només ens interessa la declinació i elevació, però trobem l'ascensió ...
+#... recta i azimut per completitud
 ra_values = []
 dec_values = []
+alt_values = []
+az_values = []
 
 for day in days:
-    observer.date = ephem.Date(day)
+    observer.date = ephem.Date(day + timedelta(hours=12))
     sun = ephem.Sun(observer)
     ra_values.append(sun.ra)
     dec_values.append(sun.dec)
+    alt_values.append(sun.alt)
+    az_values.append(sun.az)
 
 
 #Fem les conversions d'unitats necessàries
 ra_deg = np.array(ra_values) * 180 / np.pi  #Radians a graus
-dec_deg = np.array(dec_values) * 180 / np.pi  #Radians a grays
+dec_deg = np.array(dec_values) * 180 / np.pi  #Radians a graus
+alt_deg = np.array(alt_values) * 180 / np.pi  #Radians a graus
+az_deg = np.array(az_values) * 180 / np.pi  #Radians a graus
 ra_hours = ra_deg * 24 / 360  #Graus a hores
 dec_hours = dec_deg * 24 / 360  #Graus a hores
+alt_hours = alt_deg * 24 / 360  #Graus a hores
+az_hours = az_deg * 24 / 360  #Graus a hores
 
 
 #Trobem els valors de la diferència horària entre el temps solar aparent...
@@ -55,12 +63,28 @@ for d in range(len(days)):
     time_dif.append(( -7.659 * np.sin(D_aux[-1]) + 9.863 * np.sin(2 * D_aux[-1] + 3.5932) ))
 
 
-#Dibuixem l'analema del Sol a Bellaterra per a l'any 2025. 
+#Dibuixem l'analema del Sol dec(\Delta t) a l'any 2025. 
 plt.style.use('classic')
 plt.figure(figsize=(8,8), facecolor='white')
-plt.plot(time_dif, dec_hours, label="Analemma of the Sun (2025)", color='r')
+plt.plot(time_dif, dec_hours, label=r"dec(\Delta t)", color='r')
 plt.xlabel(r"$T_{aparent} - T_{mitjà}$ [$\degree$]")
 plt.ylabel("$Declinatció$ [$h$]")
+plt.title("Analema del Sol (2025)")
+ax = plt.gca()
+ax.axhline(y=0, color='black', linewidth=1)
+ax.axvline(x=0, color='black', linewidth=1)
+for spine in ax.spines.values():
+    spine.set_visible(True)
+plt.grid(True)
+plt.savefig("informe/images/analema_dec.png", dpi=300, bbox_inches='tight')
+plt.show()
+
+#Dibuixem l'analema del Sol alt(\Delta t) a Bellaterra per a l'any 2025. 
+plt.style.use('classic')
+plt.figure(figsize=(8,8), facecolor='white')
+plt.plot(time_dif, alt_deg, label=r"alt(\Delta t)", color='r')
+plt.xlabel(r"$T_{aparent} - T_{mitjà}$ [$\degree$]")
+plt.ylabel("$Elevació$ [$\degree$]")
 plt.title("Analema del Sol en Bellaterra (2025)")
 ax = plt.gca()
 ax.axhline(y=0, color='black', linewidth=1)
@@ -68,5 +92,5 @@ ax.axvline(x=0, color='black', linewidth=1)
 for spine in ax.spines.values():
     spine.set_visible(True)
 plt.grid(True)
-plt.savefig("informe/images/analema_codi.png", dpi=300, bbox_inches='tight')
+plt.savefig("informe/images/analema_alt.png", dpi=300, bbox_inches='tight')
 plt.show()
