9241a455c3e0efb3a4ea0c8e37bc2a2668054ee7
AiranReina <airanreinadelgado2004@gmail.com>
2025-05-08 14:56:48 +0200

    Just need to finish ex9

--- analema_planetes.py
+++ analema_planetes.py
@@ -1,100 +1,111 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.colors as colors
 from scipy.special import jn
+from astropy.time import Time
+from astropy.coordinates import get_body, AltAz, EarthLocation
+import astropy.units as u
 
-# Inicialitzem els paràmetres órbitals dels planetes
-planets_test = {
-    'Terra': {
-        'e': 0.0167,
-        'varpi_deg': 102.937348 + 180,
-        'eps_deg': 23.44
-    },
-    'Mercuri': {
-        'e': 0.2056,
-        'varpi_deg': 77.456 + 180,
-        'eps_deg': 0.03
-    },
-    'Mart': {
-        'e': 0.0934,
-        'varpi_deg': 336.04084 + 180,
-        'eps_deg': 25.19
-    },
-    'Júpiter': {
-        'e': 0.0489,
-        'varpi_deg': 14.75385 + 180,
-        'eps_deg': 3.13
-    },
-    'Saturn': {
-        'e': 0.0565,
-        'varpi_deg': 92.43194 + 180,
-        'eps_deg': 26.73
-    }
+# Definim els paràmetres de l'observador
+lat_deg = 41.5
+hour = 12
+lat_rad = np.radians(lat_deg)
+N = 20
+
+
+########################################
+#---------------- MART ----------------#
+########################################
+
+
+# Definim els paràmetres de Mart
+tau_mars = 16.5
+mars_period = 686.98
+days = np.linspace(0, mars_period, 1000)
+e_mars = 0.0934
+obliquity_mars = np.radians(25.19)
+perihelion_long_mars = np.radians(286.502)
+
+# Realitzem els càlculs necesàris
+M_mars = 2 * np.pi * (days - tau_mars) / mars_period
+E_series = M_mars.copy()
+for n in range(1, N + 1):
+    E_series += (2 / n) * jn(n, n * e_mars) * np.sin(n * M_mars)
+
+nu = np.arctan2(np.sqrt(1 - e_mars**2) * np.sin(E_series), np.cos(E_series) - e_mars)
+lambda_sun = (nu + perihelion_long_mars) % (2 * np.pi)
+alpha = np.unwrap(np.arctan2(np.cos(obliquity_mars) * np.sin(lambda_sun), np.cos(lambda_sun)))
+delta = np.arcsin(np.sin(obliquity_mars) * np.sin(lambda_sun))
+
+EOT_rad = M_mars - alpha
+EOT_min = EOT_rad * (1440 / (2 * np.pi))
+EOT_min -= np.mean(EOT_min)
+
+# Grafiquem l'analema vist des de Mart
+H_deg = 15 * (hour + EOT_min / 60 - 12)
+H_rad = np.radians(H_deg)
+alt = np.degrees(np.arcsin(np.sin(lat_rad) * np.sin(delta) + np.cos(lat_rad) * np.cos(delta) * np.cos(H_rad)))
+az = np.degrees(np.arctan2(-np.sin(H_rad), -np.cos(H_rad)*np.sin(lat_rad) + np.tan(delta)*np.cos(lat_rad))) % 360
+
+plt.style.use('classic')
+plt.figure(figsize=(10, 8),facecolor='white')
+sc = plt.scatter(az, alt, c=days, cmap='jet', s=100, label='Mart', edgecolors='none')
+cbar = plt.colorbar(sc, label='Día del any a Mart')
+plt.xlabel(r'Az [$\degree$]')
+plt.ylabel(r'Alt [$\degree$]')
+plt.title(f"Analema Solar a Mart a la Latitud {np.degrees(lat_rad):.1f}° (12h)", fontsize=16)
+plt.grid(True)
+plt.tight_layout()
+plt.savefig("informe/images/analema_Mart.png", dpi=300, bbox_inches='tight')
+plt.show()
+
+
+########################################
+#---------- JÚPITER I SATURN ----------#
+########################################
+
+
+# Definim els paràmetres de Júpiter i Saturn
+planets = {
+    'Júpiter': {'e': 0.0485, 'varpi_deg': 14.75385 + 180, 'eps_deg': 3.13, 'tau': 14.0, 'period': 4332.59},
+    'Saturn':  {'e': 0.0565, 'varpi_deg': 92.43194 + 180, 'eps_deg': 26.73, 'tau': 27.0, 'period': 10759.22},
 }
 
+for planet, param in planets.items():
+    
+    # Realitzem els càlculs necesàris
+    e = param['e']
+    varpi = np.radians(param['varpi_deg'])
+    eps = np.radians(param['eps_deg'])
+    tau = param['tau']
+    T = param['period']
+    days = np.linspace(0, T, 1000)
 
-# Inicialitzem les constants necesàries pel programa
-T_orbital_earth = 365.25
-tau_test = 1.07
-N_terms = 20
-days_common = np.linspace(0, T_orbital_earth, 1000)
-lat_rad = np.radians(40)
-hora_local_p = 12
-
-# Grafiquem els analemes per tots els planetes
-plt.style.use('classic')
-percent = np.linspace(0, 100, 1000)
-fig, axes = plt.subplots(1, 5, figsize=(30, 8), sharey=True, facecolor='white')
-eot_dict = {}
-radec_dict = {}
-for i, (planet, params) in enumerate(planets_test.items()):
-    e = params['e']
-    varpi = np.radians(params['varpi_deg'])
-    eps = np.radians(params['eps_deg'])
-
-    M = 2 * np.pi * (days_common - tau_test) / T_orbital_earth
+    M = 2 * np.pi * (days - tau) / T
     E = M.copy()
-    for n in range(1, N_terms + 1):
+    for n in range(1, N + 1):
         E += (2 / n) * jn(n, n * e) * np.sin(n * M)
 
     nu = np.arctan2(np.sqrt(1 - e**2) * np.sin(E), np.cos(E) - e)
     lamb = (nu + varpi) % (2 * np.pi)
-
     alpha = np.unwrap(np.arctan2(np.cos(eps) * np.sin(lamb), np.cos(lamb)))
     delta = np.arcsin(np.sin(eps) * np.sin(lamb))
 
-    alpha_deg = np.degrees(alpha) % 360
-    delta_deg = np.degrees(delta)
-    radec_dict[planet] = (alpha_deg, delta_deg)
+    EOT = (M - alpha) * (1440 / (2 * np.pi))
+    EOT -= np.mean(EOT)
 
-    EOT_rad = M - alpha
-    EOT_min = EOT_rad * (1440 / (2 * np.pi))
-    EOT_min -= np.mean(EOT_min)
-    eot_dict[planet] = EOT_min.copy()
-
-    H_deg = 15 * (hora_local_p + EOT_min / 60 - 12)
+    # Grafiquem l'analema vist des de Júpiter i Saturn
+    H_deg = 15 * (hour + EOT / 60 - 12)
     H_rad = np.radians(H_deg)
+    alt = np.degrees(np.arcsin(np.sin(lat_rad) * np.sin(delta) + np.cos(lat_rad) * np.cos(delta) * np.cos(H_rad)))
+    az = np.degrees(np.arctan2(-np.sin(H_rad),-np.cos(H_rad)*np.sin(lat_rad) + np.tan(delta)*np.cos(lat_rad))) % 360
 
-    alt = np.degrees(np.arcsin(
-        np.sin(lat_rad) * np.sin(delta) +
-        np.cos(lat_rad) * np.cos(delta) * np.cos(H_rad)
-    ))
-    az = np.degrees(np.arctan2(
-        -np.sin(H_rad),
-        -np.cos(H_rad) * np.sin(lat_rad) + np.tan(delta) * np.cos(lat_rad)
-    )) % 360
-
-    ax = axes[i]
-    sc = ax.scatter(az, alt, c=percent, cmap='jet', s=100, edgecolors='none')
-    ax.set_title(planet)
-    ax.set_xlabel(r'Az [$\degree$]')
-    ax.grid(True)
-    if i == 0:
-        ax.set_ylabel(r'Alt [$\degree$]')
-
-fig.suptitle(f"Analema Solar per Planeta a Latitud {np.degrees(lat_rad):.1f}° (Inici: 01/01/2025 a {hora_local_p}h)", fontsize=16)
-cbar = plt.colorbar(sc, label='Percentatge del any')
-cbar.set_ticks([0, 100])
-plt.tight_layout()
-plt.savefig("informe/images/analema_planetes.png", dpi=300, bbox_inches='tight')
-plt.show()
+    plt.figure(figsize=(10, 8),facecolor='white')
+    sc = plt.scatter(az, alt, c=days, cmap='jet', s=100, label='Mart', edgecolors='none')
+    cbar = plt.colorbar(sc, label=f'Día del any a {planet}')
+    plt.xlabel(r'Az [$\degree$]')
+    plt.ylabel(r'Alt [$\degree$]')
+    plt.title(f"Analema Solar a {planet} a la Latitud {np.degrees(lat_rad):.1f}° (12h)", fontsize=16)
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig(f"informe/images/analema_{planet}.png", dpi=300, bbox_inches='tight')
+    plt.show()
